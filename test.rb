3c418c542b6aa62c2c611bba36f1e7fef6d732ef
hachiya <hachiya@sein.mse.kyutech.ac.jp>
2015-11-13 04:08:42 +0000

    iroiro

--- test.rb
+++ test.rb
@@ -10,14 +10,28 @@ Prawn::Document.generate(f_name) {
 stroke_axis
 stroke_circle [0,0] , 10 #原点
 
-text "Time now"
+font_size(25)
+text "Test"
 
+# 線の描画
+stroke_color "ff0000"
+stroke do
+ # just lower the current y position
+ move_down 50
+ horizontal_rule
+ vertical_line 100, 300, :at => 50
+ horizontal_line 200, 500, :at => 150
+end
+
+#箱の中に現在時刻を表示
 bounding_box([100,300],:width=>100,:height=>100   ){
 
 	stroke_bounds
 	stroke_circle [0,0] , 10 # ボックス右下原点の相対座標になる
+	text "Current time is", :color => "FF0000"
 	text "#{t}"
 }
+
 }
 
 
