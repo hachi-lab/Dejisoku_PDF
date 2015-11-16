cec7bd2a9da752b8035fc7a6e823269a5c39cf50
hachiya <hachiya@sein.mse.kyutech.ac.jp>
2015-11-16 17:38:00 +0000

    test3

--- test3.rb
+++ test3.rb
@@ -1,13 +1,24 @@
 require "prawn"
 require "open-uri"
+require "fastimage"
+
+i_size = FastImage.size("https://upload.wikimedia.org/wikipedia/commons/7/76/Yukihiro_Matsumoto.JPG")
 
 f_name = File.basename(__FILE__, ".rb")+".pdf"
 Prawn::Document.generate(f_name) {
-#stroke_axis
+stroke_axis
+stroke_circle [0,0] , 10
 
 text "Matz"
-#image open "https://upload.wikimedia.org/wikipedia/commons/7/76/Yukihiro_Matsumoto.JPG"
+#image open "https://upload.wikimedia.org/wikipedia/commons/7/76/Yukihiro_Matsumoto.JPG", :width => 500
 
 image "Matz.jpg", :width => 500
+
+stroke do
+ # just lower the current y position
+ horizontal_rule
+ vertical_line 100, 300, :at => 50
+ horizontal_line 200, 500, :at => 150
+end
 
 }
