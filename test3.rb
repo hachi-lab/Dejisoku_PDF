f6845220f8d20a7ec4d055251aa325ba6a3a60b5
hachiya <hachiya@sein.mse.kyutech.ac.jp>
2015-11-30 18:21:19 +0000

    re

--- test3.rb
+++ test3.rb
@@ -7,7 +7,7 @@ require 'rmagick'
 img = Magick::ImageList.new("https://upload.wikimedia.org/wikipedia/commons/7/76/Yukihiro_Matsumoto.JPG")
 p ws = img.columns
 p hs = img.rows
-scale = 500.000 / ws
+scale = 700.000 / ws
 puts sprintf("%.3f", scale)
 
 f_name = File.basename(__FILE__, ".rb")+".pdf"
@@ -21,15 +21,15 @@ stroke_circle [0,0] , 10
 #text "1049"
 #image (open "https://upload.wikimedia.org/wikipedia/commons/7/76/Yukihiro_Matsumoto.JPG"), :width => 500
 
-image "1049.jpg", :width => 500
+image "1049.jpg", :width => 700
 
 stroke_color 'ff0000'
 
-stroke do
+#stroke do
  # just lower the current y position
- horizontal_rule
- vertical_line 100, 500, :at => 50
- horizontal_line 200, 500, :at => 400
-end
+ #horizontal_rule
+ #vertical_line 100, 500, :at => 50
+ #horizontal_line 200, 500, :at => 400
+#end
 
 }
