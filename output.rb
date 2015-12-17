683e17c9c2fe43f42eb69cd777f72380f63b0ec1
hachiya <hachiya@sein.mse.kyutech.ac.jp>
2015-12-17 09:30:57 +0000

    text_color

--- output.rb
+++ output.rb
@@ -29,7 +29,7 @@ slope = (myy - nyy) / (mxx - nxx)
 intercept = (nxx * myy - mxx * nyy) / (nxx - mxx)
 origin_x = 375.0
 origin_y = 525 - ($hs * $scale / 2)
-value = 15
+value = 125
 xxxxx = value * Math.cos(angle * Math::PI / 180)
 yyyyy = value * Math.sin(angle * Math::PI / 180)
 
